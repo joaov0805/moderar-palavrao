ff82b673ed882a0f90a2986cef5069443f39371c
João Victor Vasconcellos de Sousa <95720688+joaov0805@users.noreply.github.com>
2024-09-25 13:59:58 -0300

    Update Utilidades.java

--- Utilidades.java
+++ Utilidades.java
@@ -6,8 +6,42 @@ import java.util.Set;
 public class Utilidades {
 	public static Set getPalavroes() {
 		HashSet h = new HashSet();
-		h.add("casa");
-		h.add("bol");
+		h.add("merda")
+		h.add("porra")
+		h.add("caralho")
+		h.add("cacete")
+		h.add("puta")
+		h.add("puto")
+		h.add("piranha")
+		h.add("desgraça")
+		h.add("bosta")
+		h.add("foda")
+		h.add("fuder")
+		h.add("cu")
+		h.add("filho da puta")
+		h.add("arrombado")
+		h.add("buceta")
+		h.add("vagabunda")
+		h.add("vagabundo")
+		h.add("corno")
+		h.add("viado")
+		h.add("cabra safado")
+		h.add("puta que pariu")
+		h.add("puta merda")
+		h.add("vai tomar no cu")
+		h.add("fdp")
+		h.add("escroto")
+		h.add("babaca")
+		h.add("otário")
+		h.add("idiota")
+		h.add("pau no cu")
+		h.add("pau no seu cu")
+		h.add("chupa")
+		h.add("imbecil")
+		h.add("cretino")
+		h.add("imbecil")
+		h.add("desgraçado")
+
 		
 		return h;
 	}
